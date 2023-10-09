c54f8b442bd68bf7e1716ae4990b44681e2c6658
June Crane <june.crane@geogebra.at>
2023-10-08 22:30:39 -0600

    Refactor GET /pokemon endpoint to be more concise

--- server/api/pokemon.get.ts
+++ server/api/pokemon.get.ts
@@ -1,24 +1,16 @@
-import PokeAPI, { totalPokemonInAPI } from '../utils/poke-api'
+import { allPokemon, totalPokemonInAPI } from '../utils/poke-api'
 import type Pokedex from 'pokedex-promise-v2'
 
-// TODO: Add JSDoc
-// TODO: Remove hyphens from pokemon names: Do this on front-end
+/**
+ * Fetches a list of 60 pokemon. The list is always random (but still chronological) just for fun.
+ **/
 export default defineEventHandler(async (): Promise<Pokedex.NamedAPIResource[]> => {
-  // Amount of Pokemon to fetch.
   const limit = 60
+  const maxPokemon = (await totalPokemonInAPI) - limit
 
-  // Don't fetch Pokemon that don't exist
-  const maxPokemon = await totalPokemonInAPI
-  const maxFetchOffset = maxPokemon - limit
+  const offset = Math.floor(Math.random() * maxPokemon)
 
-  // Fetch a dynamic range of Pokemon, so it's different on landing/refresh (but it will still be chronological).
-  // Just for fun.
-  const offset = Math.floor(Math.random() * maxFetchOffset)
-
-  // Get and return the list of pokemon
-  // TODO: Do not re-fetch when the entire pokemon list exists on the server anyway.
-  return await PokeAPI.getPokemonsList({
-    offset, limit
-  }).then(nr => nr.results)
+  // Use the Pokemon list we have in server runtime (for search) instead of making a new request.
+  return allPokemon.then((ap) => ap.slice(offset, offset + limit))
 })
 
